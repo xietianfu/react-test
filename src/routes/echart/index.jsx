4f27fa79b3479e74780ecae7e3250458a56842a2
谢天福 <xietianfu@wolongdata.com>
2018-08-21 18:04:42 +0800

    添加echarts主题

--- src/routes/echart/index.jsx
+++ src/routes/echart/index.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import echarts from 'echarts';
+import { Button } from 'antd';
+import './wonderland';
 
 class Echart extends Component {
   constructor(props) {
     super(props);
+    this.random = Math.random() * 100000;
+    this.random = this.random.toFixed(0).toString();
     this.state = {};
   }
 
   componentDidMount() {
     // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('main'));
+    this.myChart = echarts.init(
+      document.getElementById(this.random),
+      'wonderland',
+      {
+        // 在数据量比较少的情况下使用svg，减少内存调用
+        renderer: 'svg',
+        height: '400px',
+      },
+    );
     // 绘制图表
-    myChart.setOption({
+    this.myChart.setOption({
       title: { text: 'ECharts 入门示例' },
       tooltip: {},
       xAxis: {
@@ -28,8 +40,29 @@ class Echart extends Component {
     });
   }
 
+  handleAddData = () => {
+    this.myChart.setOption({
+      xAxis: {
+        data: ['羽绒服'],
+      },
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [66],
+        },
+      ],
+    });
+  };
+
   render() {
-    return <div id="main" />;
+    const { height = '400px' } = this.props;
+    return (
+      <div>
+        <Button onClick={() => this.handleAddData()}>新增数据</Button>
+        <div id={this.random} style={{ maxWidth: '100%', height }} />
+      </div>
+    );
   }
 }
 
