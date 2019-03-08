53d75c9c8887641dd583b6bfe4bb4e7673b8fbf2
谢天福 <xietianfu@wolongdata.com>
2019-03-08 15:05:07 +0800

    chore: 完善less

--- src/container/Demo/index.jsx
+++ src/container/Demo/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../../assets/styles/pages/demo.less';
 import style from './style.less';
-import './style.less';
+import classnames from 'classnames';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 80px;
+`;
 
 const Demo = () => {
   return (
     <div>
       <h1 className="title">hello world</h1>
       <h1 className={style.title}>hello world!</h1>
-      <h1 className="new">asdlk;fjal;ksj</h1>
+      <h1 className={classnames([style.title, 'align'])}>asdlk;fjal;ksj</h1>
+      <Title className={classnames([style.title, 'align'])}>hello world</Title>
     </div>
   );
 };
